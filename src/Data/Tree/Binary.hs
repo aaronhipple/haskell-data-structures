66135f4d4f532efb3528ac85a69c388228cc7694
Aaron Hipple <aaron@bluetent.com>
2018-05-02 18:28:21 -0600

    Implement insert and stub in contains

--- src/Data/Tree/Binary.hs
+++ src/Data/Tree/Binary.hs
@@ -1,6 +1,23 @@
-module Data.Tree.Binary (BinaryTree(..))  where
+module Data.Tree.Binary (BinaryTree(..), insert, contains)  where
 
 data BinaryTree a
   = Empty
   | Leaf a (BinaryTree a) (BinaryTree a)
   deriving (Eq, Show)
+
+insert :: Ord a => a -> BinaryTree a -> BinaryTree a
+insert newValue leaf =
+  case leaf of
+    Empty -> Leaf newValue Empty Empty
+    Leaf currentValue left right ->
+      if currentValue > newValue then
+        case left of
+          Empty -> Leaf currentValue (Leaf newValue Empty Empty) right
+          _ -> Leaf currentValue (insert newValue left) right
+      else
+        case right of
+          Empty -> Leaf currentValue left (Leaf newValue Empty Empty)
+          _ -> Leaf currentValue left (insert newValue left)
+
+contains :: Ord a => a -> BinaryTree a -> Bool
+contains value tree = False
--- test/Data/Tree/BinarySpec.hs
+++ test/Data/Tree/BinarySpec.hs
@@ -15,4 +15,16 @@ spec = do
   describe "BinaryTree" $ do
     it "constructs successfully" $ do
       Leaf 0 Empty Empty `shouldBe` Leaf 0 Empty Empty
-
+  describe "insert" $ do
+    it "inserts an item into an empty tree" $ do
+      insert 5 Empty `shouldBe` Leaf 5 Empty Empty
+    it "inserts a lesser item to the left" $ do
+      insert 3 (Leaf 5 Empty Empty) `shouldBe` Leaf 5 (Leaf 3 Empty Empty) Empty
+    it "inserts a greater item to the right" $ do
+      insert 7 (Leaf 5 Empty Empty) `shouldBe` Leaf 5 Empty (Leaf 7 Empty Empty)
+    it "recursively inserts an item" $ do
+      insert 4 (Leaf 5 (Leaf 3 Empty Empty) Empty) `shouldBe` Leaf 5 (Leaf 3 Empty (Leaf 4 Empty Empty)) Empty
+  describe "contains" $ do
+    it "determines if a tree contains a value" $ do
+     contains 5 (Leaf 5 (Leaf 3 Empty (Leaf 4 Empty Empty)) Empty) `shouldBe` True
+     contains 9 (Leaf 5 (Leaf 3 Empty (Leaf 4 Empty Empty)) Empty) `shouldBe` False
