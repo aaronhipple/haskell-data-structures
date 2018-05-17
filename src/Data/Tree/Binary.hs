1b6f116fe409df213e0aa71b15174b36757d12db
Aaron Hipple <aaron@bluetent.com>
2018-05-16 19:24:15 -0600

    Implement fromList and sort

--- src/Data/Tree/Binary.hs
+++ src/Data/Tree/Binary.hs
@@ -1,31 +1,37 @@
-module Data.Tree.Binary (BinaryTree(..), insert, contains, inOrder)  where
+module Data.Tree.Binary
+  ( BinaryTree(..)
+  , insert
+  , contains
+  , inOrder
+  , sort
+  ) where
 
 data BinaryTree a
   = Empty
-  | Leaf a (BinaryTree a) (BinaryTree a)
+  | Leaf a
+         (BinaryTree a)
+         (BinaryTree a)
   deriving (Eq, Show)
 
 insert :: Ord a => a -> BinaryTree a -> BinaryTree a
 insert newValue Empty = Leaf newValue Empty Empty
 insert newValue (Leaf currentValue left right)
-  | currentValue > newValue =
-    case left of
-      Empty -> Leaf currentValue (Leaf newValue Empty Empty) right
-      _ -> Leaf currentValue (insert newValue left) right
-  | otherwise =
-    case right of
-      Empty -> Leaf currentValue left (Leaf newValue Empty Empty)
-      _ -> Leaf currentValue left (insert newValue left)
+  | currentValue > newValue = (Leaf currentValue (insert newValue left) right)
+  | otherwise = (Leaf currentValue left (insert newValue right))
 
-contains :: Ord a => a  -> BinaryTree a -> Bool
+contains :: Ord a => a -> BinaryTree a -> Bool
 contains _ Empty = False
-contains value (Leaf checkValue left right) 
+contains value (Leaf checkValue left right)
   | checkValue == value = True
-  | checkValue < value  = contains value right
-  | checkValue > value  = contains value left
+  | checkValue < value = contains value right
+  | checkValue > value = contains value left
 
-
-
-inOrder :: Ord a => BinaryTree a -> [a] 
+inOrder :: Ord a => BinaryTree a -> [a]
 inOrder Empty = []
 inOrder (Leaf value left right) = concat [inOrder left, [value], inOrder right]
+
+fromList :: Ord a => [a] -> BinaryTree a
+fromList = foldr insert Empty
+
+sort :: Ord a => [a] -> [a]
+sort xs = inOrder $ fromList xs
